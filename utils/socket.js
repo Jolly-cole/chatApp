9452090be03308a35b353e55bb4e8d36a6515c8f
Jolly-cole <vrwt22@gmail.com>
2023-01-11 17:47:12 +0530

    first commit

--- utils/socket.js
+++ utils/socket.js
@@ -1,5 +1,5 @@
-import { io } from "socket.io-client";
-const socket = io.connect("http://192.168.0.100:4000",{reconnect: true}); // here
+import {io} from 'socket.io-client';
+const socket = io.connect('http://192.168.0.104:4000', {reconnect: true}); // here
 export default socket;
 
 // this address, it should be localhost, or my IP? wait
